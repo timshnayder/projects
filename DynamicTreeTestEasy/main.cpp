a32d37e03b87ff1efed6223c883ca888027c0e9e
Timothy Shnayder <timshnayder@gmail.com>
2023-10-15 00:26:20 -0400

    Working on DynamicTreeTestEasy, version is suboptimal but working (now uses Euler's tour and sparse table)

--- DynamicTreeTestEasy/main.cpp
+++ DynamicTreeTestEasy/main.cpp
@@ -2,28 +2,37 @@
 #include <vector>
 #include <memory>
 #include <list>
-#define MAXNODES 100001
+#include <unordered_map>
+
 using namespace std;
 
 class Node{
 public:
-    Node():weight(0), index(0), parent(nullptr){
+    Node():weight(0), index(0), eulerTourIndex(0), firstEulerAppearance(-1), parent(nullptr){
 
     }
     int weight;
     int index;
+    int eulerTourIndex;
+    int firstEulerAppearance;
     Node* parent;
-    list<Node*> neighbours;
+    unordered_map<int, Node*> neighbours;
 };
 
 class Tree{
 private:
     Node* root;
     Node* nodes;
+    int numberOfVertices;
+    vector<int> eulerTour;
+    vector<int> eulerIndexToNodeIndex;
+    int eulerTourIndex;
     void setParent(Node* node, Node* parent);
-    void getPathToRoot(int startIndex, vector<Node*>& path);
+    void buildRMQTables();
+    vector<int> logTable;
+    vector<vector<int>> sparseTable;
 public:
-    Tree(int nVertices):root(nullptr), nodes(new Node[nVertices]){
+    Tree(int nVertices):root(nullptr), nodes(new Node[nVertices]), numberOfVertices(nVertices), eulerTourIndex(0){
 
     }
     ~Tree(){
@@ -38,112 +47,181 @@ public:
     void addEdge(int firstIndex, int secondIndex){
         Node& firstNode = nodes[firstIndex - 1];
         Node& secondNode = nodes[secondIndex - 1];
-        firstNode.neighbours.push_back(&secondNode);
-        secondNode.neighbours.push_back(&firstNode);
+        firstNode.neighbours[secondIndex] = &secondNode;
+        secondNode.neighbours[firstIndex] = &firstNode;
     }
 
     void changeParent(int nodeIndex, int newParentIndex){
         //Remove this child from old parent and remove parent from child
         Node* node = &nodes[nodeIndex-1];
         if(node->parent!= nullptr){
-            list<Node*> &parentNeighbours = node->parent->neighbours;
-            for(auto neighbour = parentNeighbours.begin(); neighbour != parentNeighbours.end(); neighbour++){
-                if(*neighbour==node){
-                    parentNeighbours.erase(neighbour);
-                    break;
-                }
-            }
-            for(auto neighbour = node->neighbours.begin(); neighbour != node->neighbours.end(); neighbour++){
-                if(*neighbour==node->parent){
-                    node->neighbours.erase(neighbour);
-                    break;
-                }
-            }
+            node->parent->neighbours.erase(nodeIndex);
+            node->neighbours.erase(node->parent->index);
         }
 
         //Connect node and new parent
         addEdge(nodeIndex, newParentIndex);
 
-
         //Change nodes parent to new parent
         Node* newParent = &nodes[newParentIndex-1];
         node->parent = newParent;
-
+        setRoot(root->index);
     }
 
     void setRoot(int rootIndex);
-    void findPath(int start, int end, vector<Node*> &path);
     Node* LCA(int start, int end);
-};
 
-void Tree::getPathToRoot(int startIndex, vector<Node*> &path) {
-    Node* node = &nodes[startIndex-1];
-    while(node!= nullptr){
-        path.push_back(node);
+    void updatePath(int start, int end, int weight);
+    void incrementPath(int start, int end, int weight);
+    int minPath(int start, int end);
+    int maxPath(int start, int end);
+    int sumPath(int start, int end);
+};
+//Query = 1
+void Tree::updatePath(int start, int end, int weight) {
+    Node* lca = LCA(start, end);
+    Node* node = &nodes[start-1];
+    while(node!=lca){
+        node->weight = weight;
         node = node->parent;
     }
+    node = &nodes[end-1];
+    while(node!=lca){
+        node->weight = weight;
+        node = node->parent;
+    }
+    lca->weight=weight;
+}
+
+//Query = 2
+void Tree::incrementPath(int start, int end, int weight) {
+    Node* lca = LCA(start, end);
+    Node* node = &nodes[start-1];
+    while(node!=lca){
+        node->weight += weight;
+        node = node->parent;
+    }
+    node = &nodes[end-1];
+    while(node!=lca){
+        node->weight += weight;
+        node = node->parent;
+    }
+    lca->weight+=weight;
+}
+
+//Query = 3
+int Tree::minPath(int start, int end) {
+    Node* lca = LCA(start, end);
+    int min = lca->weight;
+    Node* node = &nodes[start-1];
+    while(node!=lca){
+        if(node->weight<min){
+            min = node->weight;
+        }
+        node = node->parent;
+    }
+    node = &nodes[end-1];
+    while(node!=lca){
+        if(node->weight<min){
+            min = node->weight;
+        }
+        node = node->parent;
+    }
+    return min;
+}
+
+//Query = 4
+int Tree::maxPath(int start, int end) {
+    Node* lca = LCA(start, end);
+    int max = lca->weight;
+    Node* node = &nodes[start-1];
+    while(node!=lca){
+        if(node->weight>max){
+            max = node->weight;
+        }
+        node = node->parent;
+    }
+    node = &nodes[end-1];
+    while(node!=lca){
+        if(node->weight>max){
+            max = node->weight;
+        }
+        node = node->parent;
+    }
+    return max;
+}
+
+//Query = 5
+int Tree::sumPath(int start, int end) {
+    Node* lca = LCA(start, end);
+    int sum = lca->weight;
+    Node* node = &nodes[start-1];
+    while(node!=lca){
+        sum += node->weight;
+        node = node->parent;
+    }
+    node = &nodes[end-1];
+    while(node!=lca){
+        sum += node->weight;
+        node = node->parent;
+    }
+    return sum;
 }
 
 void Tree::setParent(Node* node, Node *parent) {
+    if(node->firstEulerAppearance == -1){
+        node->firstEulerAppearance = eulerTour.size();
+        node->eulerTourIndex = eulerTourIndex++;
+        eulerIndexToNodeIndex.push_back(node->index);
+    }
+    eulerTour.push_back(node->eulerTourIndex);
     node->parent = parent;
     for(auto neighbour: node->neighbours){
-        if(neighbour!=parent){
-            setParent(neighbour, node);
+        if(neighbour.second!=parent){
+            setParent(neighbour.second, node);
+            eulerTour.push_back(node->eulerTourIndex);
         }
     }
 }
 
 void Tree::setRoot(int rootIndex) {
+    eulerTour.clear();
+    eulerIndexToNodeIndex.clear();
+    for(int i = 0; i < numberOfVertices; i++){
+        nodes[i].firstEulerAppearance = -1;
+    }
+    eulerTourIndex = 0;
     root = &nodes[rootIndex-1];
     setParent(root, nullptr);
+    buildRMQTables();
 }
 
-void Tree::findPath(int start, int end, vector<Node*> &path){
-    vector<Node*> firstPath;
-    getPathToRoot(start, firstPath);
-    vector<Node*> secondPath;
-    getPathToRoot(end, secondPath);
-    int length = min(firstPath.size(), secondPath.size());
-
-    path.reserve(firstPath.size()+secondPath.size());
-    for(int i = 0; i < length; i++){
-        int firstIndex = firstPath.size()-i-1;
-        int secondIndex = secondPath.size()-i-1;
-        if(firstPath[firstIndex]!=secondPath[secondIndex]){
-            path.insert(path.end(), firstPath.begin(), firstPath.begin()+firstIndex+2);
-            path.insert(path.end(), secondPath.rbegin()+i, secondPath.rend());
-            break;
-        }
+void Tree::buildRMQTables(){
+    logTable.assign(eulerTour.size() + 1, 0);
+    for (int i = 2; i < logTable.size(); i++) {
+        logTable[i] = logTable[i / 2] + 1;
     }
-
-    if(path.empty()){
-        //The shorter path is subset of longer path, so they never diverge and previous loop never finds the difference
-        if(firstPath.size() > secondPath.size()){
-            path.insert(path.end(), firstPath.begin(), firstPath.end()-secondPath.size()+1);
-        } else {
-            path.insert(path.end(), secondPath.rbegin()+firstPath.size()-1, secondPath.rend());
+    sparseTable.assign(logTable.back() + 1, vector<int>(eulerTour.size()));
+    sparseTable[0] = eulerTour;
+    for(int row = 1; row < sparseTable.size(); row++) {
+        for (int i = 0; i + (1 << row) <= eulerTour.size(); i++) {
+            sparseTable[row][i] = min(sparseTable[row - 1][i], sparseTable[row - 1][i + (1 << (row - 1))]);
         }
     }
 }
 
 Node* Tree::LCA(int start, int end){
-    vector<Node*> firstPath;
-    getPathToRoot(start, firstPath);
-    vector<Node*> secondPath;
-    getPathToRoot(end, secondPath);
-    int length = min(firstPath.size(), secondPath.size());
-    for(int i = 0; i < length; i++){
-        int firstIndex = firstPath.size()-i-1;
-        int secondIndex = secondPath.size()-i-1;
-        if(firstPath[firstIndex]!=secondPath[secondIndex]){
-            return firstPath[firstIndex]->parent;
-        }
+    if(start==end){
+        return &nodes[start-1];
     }
-    if(firstPath.size() > secondPath.size()){
-        return *(firstPath.rbegin()+(secondPath.size()-1));
-    }else{
-        return *(secondPath.rbegin()+(firstPath.size()-1));
-    }
+    start=nodes[start-1].firstEulerAppearance;
+    end=nodes[end-1].firstEulerAppearance;
+    int left = min(start, end);
+    int right = max(start, end);
+    int log = logTable[right - left];
+    int eulerIndex = min(sparseTable[log][left], sparseTable[log][right - (1 << log)]);
+    int nodeIndex = eulerIndexToNodeIndex[eulerIndex];
+    return &nodes[nodeIndex-1];
 }
 
 int main() {
@@ -178,53 +256,22 @@ int main() {
             tree.setRoot(root);
         } else if(query == 1){
             cin >> x >> y >> z;
-            vector<Node*> path;
-            tree.findPath(x,y, path);
-            for(Node* node: path){
-                node->weight=z;
-            }
+            tree.updatePath(x,y, z);
         } else if(query == 2){
             cin >> x >> y >> z;
-            vector<Node*> path;
-            tree.findPath(x,y, path);
-            for(Node* node: path){
-                node->weight+=z;
-            }
+            tree.incrementPath(x, y, z);
         } else if(query == 3){
             cin >> x >> y;
-            vector<Node*> path;
-            tree.findPath(x,y, path);
-            int min = path[0]->weight;
-            for(Node* node: path){
-                if(node->weight<min){
-                    min = node->weight;
-                }
-            }
-            cout << min << endl;
+            cout << tree.minPath(x,y) << endl;
         } else if(query == 4){
             cin >> x >> y;
-            vector<Node*> path;
-            tree.findPath(x,y, path);
-            int max = path[0]->weight;
-            for(Node* node: path){
-                if(node->weight>max){
-                    max = node->weight;
-                }
-            }
-            cout << max << endl;
+            cout << tree.maxPath(x,y) << endl;
         } else if(query == 5){
             cin >> x >> y;
-            vector<Node*> path;
-            tree.findPath(x,y, path);
-            int sum = 0;
-            for(Node* node: path){
-                sum+=node->weight;
-            }
-            cout << sum << endl;
+            cout << tree.sumPath(x,y) << endl;
         } else if(query == 6){
             cin >> x >> y;
             tree.changeParent(x,y);
-
         } else if(query == 7){
             cin >> x >> y;
             Node* lca = tree.LCA(x, y);
