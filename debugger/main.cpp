94fb62853875c8c3ebb8f4dcde2a7d0a12b1c1a8
Timothy Shnayder <timshnayder@gmail.com>
2024-02-15 18:34:51 -0500

    added projects

--- debugger/main.cpp
+++ debugger/main.cpp
@@ -7,22 +7,27 @@ using namespace std;
 
 int main(){
     ll n, m, k;
+    // n = number of notes
+    // m = max pitch
+    // k = number of good samples
+
     cin >> n >> m >> k;
     ll music[n];
-    music[n-1] = -1;
-
-    for(int i = 0; i < n; i++){
+    for(ll i = 0; i < n; i++){
         //plan
         /*
          * Build up music until only putting distinct numbers
          * 1 2 3 1 1 1 1
          */
-        // 1 2 3: GOOD SAMPLES = SUM OF NUMBERS
-        ll remaining = n-i-1;
+        // 1 2 3: GOOD SAMPLES = SUM OF NUMBERS = 6
+        ll remaining = n-i-1; // Number of remaining notes
         ll current = min(m,k-remaining);
+        if(current<=0){
+            break;
+        }
         ll val;
         if(current>i){
-            val = min(m, (ll)i+1);
+            val = min(m, i+1);
             current = val;
         }else{
             val = music[i-current];
